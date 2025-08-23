b3a1d08a8d624e43412960dab9534c38a95dd869
Zicheng Wang <16488024+zikowang@users.noreply.github.com>
2025-08-23 23:50:19 +0200

    chore: home page cards

--- src/components/PageComponents/HomePage/ShortLinks.tsx
+++ src/components/PageComponents/HomePage/ShortLinks.tsx
@@ -1,5 +1,6 @@
 /** @format */
 
+import { CardHorizontal } from "@/components/ui/card-horizontal";
 import CardVertical from "@/components/ui/card-vertical";
 import { useColorMode } from "@/components/ui/color-mode";
 import { Badge, Stack } from "@chakra-ui/react";
@@ -8,35 +9,87 @@ const ShortLinks = () => {
     const { colorMode } = useColorMode();
 
     return (
-        <Stack gap={4} alignItems="flex-start" direction="row" wrap="wrap" mt={8}>
-            <CardVertical
-                title="Calendar"
-                img={`/img/kingshot-calendar.png`}
-                alt="calender teaser image"
-                description="Useful overview of all upcoming events and their schedule."
-                href="/calendar"
-                badges={
-                    <>
-                        <Badge colorPalette="blue">Events</Badge>
-                        <Badge colorPalette="green">Planning</Badge>
-                    </>
-                }
-            />
+        <>
+            <Stack
+                gap={4}
+                alignItems="flex-start"
+                direction="row"
+                wrap="wrap"
+                mt={8}
+                display={{
+                    base: "none",
+                    md: "flex",
+                }}
+            >
+                <CardVertical
+                    title="Calendar"
+                    img={`/img/kingshot-calendar.png`}
+                    alt="calender teaser image"
+                    description="Useful overview of all upcoming events and their schedule."
+                    href="/calendar"
+                    badges={
+                        <>
+                            <Badge colorPalette="blue">Events</Badge>
+                            <Badge colorPalette="green">Planning</Badge>
+                        </>
+                    }
+                />
 
-            <CardVertical
-                title="Troop Calculator"
-                img={`/img/kingshot-archer.png`}
-                alt="troops calculator teaser image"
-                description="Find out how many speed ups and resources you need for your troop training and promotions."
-                href="/calculators/troop"
-                badges={
-                    <>
-                        <Badge colorPalette="red">Troops</Badge>
-                        <Badge colorPalette="purple">Calculator</Badge>
-                    </>
-                }
-            />
-        </Stack>
+                <CardVertical
+                    title="Troop Calculator"
+                    img={`/img/kingshot-archer.png`}
+                    alt="troops calculator teaser image"
+                    description="Find out how many speed ups and resources you need for your troop training and promotions."
+                    href="/calculators/troop"
+                    badges={
+                        <>
+                            <Badge colorPalette="red">Troops</Badge>
+                            <Badge colorPalette="purple">Calculator</Badge>
+                        </>
+                    }
+                />
+            </Stack>
+
+            <Stack
+                gap={4}
+                alignItems="flex-start"
+                direction="row"
+                wrap="wrap"
+                mt={8}
+                display={{
+                    base: "flex",
+                    md: "none",
+                }}
+            >
+                <CardHorizontal
+                    title="Calendar"
+                    img={`/img/kingshot-calendar.png`}
+                    alt="calender teaser image"
+                    description="Useful overview of all upcoming events and their schedule."
+                    href="/calendar"
+                    badges={
+                        <>
+                            <Badge colorPalette="blue">Events</Badge>
+                            <Badge colorPalette="green">Planning</Badge>
+                        </>
+                    }
+                />
+
+                <CardHorizontal
+                    title="Troop Calculator"
+                    img={`/img/kingshot-archer.png`}
+                    alt="troops calculator teaser image"
+                    description="Find out how many speed ups and resources you need for your troop training and promotions."
+                    href="/calculators/troop"
+                    badges={
+                        <>
+                            <Badge colorPalette="red">Troops</Badge>
+                            <Badge colorPalette="purple">Calculator</Badge>
+                        </>
+                    }
+                />
+            </Stack>
+        </>
     );
 };
 
