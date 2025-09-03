0d4fe061582042cbd313712015a44ec508aa3b53
Zicheng Wang <16488024+zikowang@users.noreply.github.com>
2025-09-03 08:06:02 +0200

    chore: calendar more information

--- src/data/events/officerProject.ts
+++ src/data/events/officerProject.ts
@@ -1,7 +1,7 @@
 /** @format */
 
 import type { Event } from "@/types/events";
-import { forgeHammer, gems, mythicExpeditionSkillManual } from "../items";
+import { charmDesign, forgeHammer, gems, mythicExpeditionSkillManual } from "../items";
 
 export const officerProject: Event = {
     id: "officer-project",
@@ -10,6 +10,6 @@ export const officerProject: Event = {
     image: "/img/100x100/events/kingshot-officer-project-event-icon.png",
     color: "#528ec7",
     days: [3, 4, 7, 8, 17, 18, 21, 22],
-    rewards: [gems, forgeHammer, mythicExpeditionSkillManual],
-    todo: [],
+    rewards: [gems, forgeHammer, charmDesign, mythicExpeditionSkillManual],
+    todo: ["Raise Governor Charm", "Train Troops", "Use Forgehammer", "Use Widgets"],
 };
